013ca6ff259c363483c2502de9cc145856763e4e
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-08-29 01:47:42 -0700

    rubyのtest実行ファイル作成

---
+++ test.rb
@@ -0,0 +1,32 @@
+# 文字列カウント
+# p 'hoge'.length
+
+# s = 'hoge'
+# if s.empty?
+#     p 'yes'
+# else 
+#     p 'nononono'
+# end
+
+# メソッド定義
+# def string_message(str = '')
+#   if str.empty?
+#     "It's an empty string!"
+#   else
+#     "The string is nonempty."
+#   end
+# end
+# puts string_message("foobar")
+
+
+# 配列
+# p "foo bar baz".split
+
+# (1..5).each do |i|
+#     puts 2 * i
+# end
+
+
+# ハッシュ
+user = { "first_name" => "Michael", "last_name" => "Hartl" }
+p user
