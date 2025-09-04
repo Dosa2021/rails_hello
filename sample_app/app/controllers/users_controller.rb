5c2a89928bfbc11aa0144a36ba056eec820d7503
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-09-03 22:01:02 -0700

    デザイン修正

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "アカウントを作成しました。"
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
