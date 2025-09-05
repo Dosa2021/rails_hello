74482bbd83226c136eb5486fab68a68a51618977
Dosa2021 <69082197+Dosa2021@users.noreply.github.com>
2025-09-04 20:16:39 -0700

    8. 基本的なログイン (#5)
    * sessionコントローラ作成
    
    * ログイン実装
    
    * アカウント作成と同時にログイン
    
    * ログアウト実装
    
    * スタイル変更
    
    * ログインボーナス実装
    
    * 修正
    
    * トップページ修正
    
    * プロフィールページ修正
    
    * レイアウト修正
    
    * テスト修正
    
    ---------
    
    Co-authored-by: dosaken <dosa@dosakennoMacBook-Pro.local>

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-class ApplicationController < ActionController::Base    
-    def hello
-        render html: "hello, tatsuya"
-    end
+class ApplicationController < ActionController::Base
+  include SessionsHelper
 end
---
+++ sample_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      reset_session # ログインの直前に必ずこれを書くこと
+      log_in user
+      redirect_to user
+    else
+      # nowメソッドを使用してメッセージを一時的なものにする
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    # httpステータスは see_other
+    redirect_to root_url, status: :see_other
+  end  
+end
