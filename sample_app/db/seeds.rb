7ad0f722c785c7b3dc7efdf244210af9f1a82272
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-09-07 21:31:35 -0700

    一旦コメントアウト

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -16,12 +16,12 @@ User.create!(name:  "Example User",
              admin: true)
 
 # 追加のユーザーをまとめて生成する
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password)
+# end
