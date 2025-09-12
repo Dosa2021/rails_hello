09a1600dddc54cd43d004ebe10363c15b951ff61
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-09-11 22:46:15 -0700

    フォロー・フォロワーページ作成

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [
+    :index,
+    :edit,
+    :update,
+    :destroy,
+    :following,
+    :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -50,6 +56,22 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url, status: :see_other
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    # TODO: ページネーション
+    @users = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    # TODO: ページネーション
+    @users = @user.followers
+    render 'show_follow'
   end
 
   private
