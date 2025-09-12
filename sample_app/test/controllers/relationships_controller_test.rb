926ada2d4a6bd1f98f9f9363c107183684d99a80
dosaken <dosa@dosakennoMacBook-Pro.local>
2025-09-11 23:20:14 -0700

    Hotwire実装

--- sample_app/test/controllers/relationships_controller_test.rb
+++ sample_app/test/controllers/relationships_controller_test.rb
@@ -3,10 +3,13 @@ require "test_helper"
 class RelationshipsControllerTest < ActionDispatch::IntegrationTest
 
   test "create should require logged-in user" do
-    assert_no_difference 'Relationship.count' do
-      post relationships_path
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.turbo_stream
     end
-    assert_redirected_to login_url
+
   end
 
   test "destroy should require logged-in user" do
